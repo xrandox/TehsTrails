64ea4c3132638c983225b6317c6e4cda14719c06
xrandox <xrandox17@yahoo.com>
2023-02-23 05:27:13 -0600

    reset(), added expiration (5min)

--- TehsTrails/Data/TehsTrails/Scripts/highlight.lua
+++ TehsTrails/Data/TehsTrails/Scripts/highlight.lua
@@ -2,38 +2,50 @@ Teh.highlight = {
     currentWaypoint = nil,
     waypointHighlighted = false,
     currentSize = 100,
-    sizeGrowing = true
+    sizeGrowing = true,
+    timeSinceStarted = 0
 }
 
 Debug:Watch("highlight", Teh.highlight)
 
+local function Teh_Highlight_Reset()
+    local waypoint = Teh.highlight.currentWaypoint
+    waypoint.MapVisibility = false
+    waypoint.MiniMapVisibility = false
+    Teh.highlight.waypointHighlighted = false
+end
+
 --Sets the given marker GUID as the current waypoint and highlights it
 function Teh_Highlight_Waypoint(marker, isfocused, markerguid)
     if (not World:CategoryByType("tt.mc.s.ewh"):IsVisible()) then return end
-    
+
     local waypoint = World:MarkerByGuid(markerguid)
 
     if (isfocused) then
-        if (Teh.highlight.waypointHighlighted) then 
-            Teh.highlight.currentWaypoint.MapVisibility = false
-            Teh.highlight.currentWaypoint.MiniMapVisibility = false
+        if (Teh.highlight.waypointHighlighted) then
+            Teh_Highlight_Reset()
         end
         waypoint:SetTexture("Data/TehsTrails/Markers/waypoint-highlight.png")
         waypoint.MapVisibility = true
         waypoint.MiniMapVisibility = true
         Teh.highlight.currentWaypoint = waypoint
         Teh.highlight.waypointHighlighted = true
+        Teh.highlight.timeSinceStarted = 0
     end
 
 end
 
 --Grows or shrinks the highlight on the map
 function Teh_Tick_Highlight(gameTime)
+    local elapsedSeconds = gameTime.ElapsedGameTime.TotalSeconds
+    Teh.highlight.timeSinceStarted = Teh.highlight.timeSinceStarted + elapsedSeconds
+    if (Teh.highlight.timeSinceStarted > 300) then Teh_Highlight_Reset() return end
+
     local size = Teh.highlight.currentSize
     local growing = Teh.highlight.sizeGrowing
     local MAX_SIZE = 200
     local MIN_SIZE = 100
-    local INTERVAL = 180 * gameTime.ElapsedGameTime.TotalSeconds
+    local INTERVAL = 180 * elapsedSeconds
 
     if (growing) then size = size + INTERVAL;
     else size = size - INTERVAL; end
@@ -50,14 +62,10 @@ end
 --Returns true if the waypoint is still highlighted
 function Teh_Validate_Highlight()
     local waypoint = Teh.highlight.currentWaypoint
-
     if (waypoint.Focused) then
-        waypoint:SetTexture("Data/TehsTrails/Markers/Waypoint.png")
-        waypoint.MapVisibility = false
-        waypoint.MiniMapVisibility = false
-        Teh.highlight.waypointHighlighted = false
+            Teh_Highlight_Reset()
         return false
     end
-
     return true
 end
+
