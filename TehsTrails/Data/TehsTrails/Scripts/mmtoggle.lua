1a42c2417e53dad97bdf72de1f30f807ec60340f
xrandox <xrandox17@yahoo.com>
2023-06-06 15:24:17 -0500

    mmtoggle bugfix

--- TehsTrails/Data/TehsTrails/Scripts/mmtoggle.lua
+++ TehsTrails/Data/TehsTrails/Scripts/mmtoggle.lua
@@ -1,16 +1,19 @@
 -- Sets the 
 function Teh_SetMinimapVisibility()
     local mainTrail = World:TrailByGuid("cb6VPxgSL0yytSwws/y+Vg==")
+    if (mainTrail == nil) then return end
     mainTrail.MiniMapVisibility = Teh_Is_True("minimapToggled")
 end
 
 function Teh_SetHeartZoneMapVisibility()
     local heartZone = World:TrailByGuid("cb6VPxgSL0yytSwws/y+Pg==")
+    if (heartZone == nil) then return end
     heartZone.MapVisibility = Teh_Is_True("heartZonesOnMap")
 end
 
 function Teh_SetHeartZoneMinimapVisibility()
     local heartZone = World:TrailByGuid("cb6VPxgSL0yytSwws/y+Pg==")
+    if (heartZone == nil) then return end
     heartZone.MiniMapVisibility = Teh_Is_True("heartZonesOnMinimap")
 end
 
