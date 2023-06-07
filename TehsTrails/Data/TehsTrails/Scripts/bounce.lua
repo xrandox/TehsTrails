ecf91adb0126faea8fbd2bcea4e39623d194d37f
xrandox <xrandox17@yahoo.com>
2023-06-07 00:44:49 -0500

    bounce bugfix

--- TehsTrails/Data/TehsTrails/Scripts/bounce.lua
+++ TehsTrails/Data/TehsTrails/Scripts/bounce.lua
@@ -7,28 +7,26 @@ Teh.bounce = {
 
 Debug:Watch("bounce", Teh.bounce)
 
-local default = Teh.bounce
-
 -- Reset the bouncing marker to normal
 function Teh_BounceReset()
     local target = Teh.bounce.currentTarget
     local bounce = target:GetBehavior("BounceModifier")
     bounce.BounceHeight = 0
+    Teh.bounce.isBouncing = false
     local category = target.Category.Name
     if (category == "1") then
         target:SetTexture("Data/TehsTrails/Markers/1.png")
     elseif (category == "2") then
         target:SetTexture("Data/TehsTrails/Markers/2.png")
-    else
+    elseif (category == "3") then
         target:SetTexture("Data/TehsTrails/Markers/3.png")
     end
-    Teh.bounce = default
 end
 
 -- Checks if the player enters the markers trigger range, if they do, waits for them to no longer be in the trigger range to stop it from bouncing
 function Teh_BounceTickHandler(gameTime)
-    if (not Teh_GetBool("bounceToggled")) then Teh_BounceReset() return end
     Teh.bounce.timeSinceStarted = Teh.bounce.timeSinceStarted + gameTime.ElapsedGameTime.TotalSeconds
+    if (not Teh_GetBool("bounceToggled")) then Teh_BounceReset() return end
     if (Teh.bounce.timeSinceStarted > 300) then Teh_BounceReset() return end
 
     if (Teh.bounce.wasFocused) then
@@ -38,6 +36,7 @@ function Teh_BounceTickHandler(gameTime)
         end
         return
     end
+
     if (Teh.bounce.currentTarget.Focused) then
         Teh.bounce.wasFocused = true
     end
@@ -57,9 +56,11 @@ function Teh_Bounce(marker, isAutoTrigger, guid)
         target:SetTexture("Data/TehsTrails/Markers/1-shadow.png")
     elseif (category == "2") then
         target:SetTexture("Data/TehsTrails/Markers/2-shadow.png")
-    else
+    elseif (category == "3") then
         target:SetTexture("Data/TehsTrails/Markers/3-shadow.png")
     end
     Teh.bounce.currentTarget = target
     Teh.bounce.isBouncing = true
+    Teh.bounce.wasFocused = false
+    Teh.bounce.timeSinceStarted = 0
 end
