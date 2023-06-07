33ffa19a1ca358acd03ac03883bc08106fd979bc
xrandox <xrandox17@yahoo.com>
2023-06-06 22:53:01 -0500

    moved hide/reset

--- TehsTrails/Data/TehsTrails/Scripts/hide.lua
+++
@@ -1,6 +0,0 @@
--- Hides the script reminders category (if the script runs, that means you have scripts enabled and dont need a reminder!)
-function Hide_reminders()
-    World.CategoryByType("tt.mc.cm.se"):Hide()
-end
-
-Hide_reminders()
--- TehsTrails/Data/TehsTrails/Scripts/reset.lua
+++
@@ -1,7 +0,0 @@
-function Teh_Reset_Scripts()
-    if (Teh.bounce.isBouncing) then Teh_Bounce_Reset() end
-    if (Teh.heartfollower.isFollowing) then Teh_Follower_Reset() end
-    if (Teh.highlight.waypointHighlighted) then Teh_Highlight_Reset() end
-    saveValue("hasSkyscale", "false")
-    saveValue("trailColor", "Light Blue [Default]")
-end
