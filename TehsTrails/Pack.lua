4eb495da30eb9f9244dbf97c60e9416df00f0e86
xrandox <xrandox17@yahoo.com>
2023-06-06 22:55:01 -0500

    cleanup

--- TehsTrails/Pack.lua
+++ TehsTrails/Pack.lua
@@ -1,14 +1,12 @@
 Teh = {}
 
-Debug:Print("Loading Lua")
+-- Hides the script reminders category (if the script runs, that means you have scripts enabled and dont need a reminder!)
+World.CategoryByType("tt.mc.cm.se"):Hide()
 
 -- Loading storage first
 Pack:Require("Data/TehsTrails/Scripts/storage.lua")
 Pack:Require("Data/TehsTrails/Scripts/trailcolors.lua")
-Pack:Require("Data/TehsTrails/Scripts/hide.lua")
 Pack:Require("Data/TehsTrails/Scripts/lookups.lua")
-Pack:Require("Data/TehsTrails/Scripts/reset.lua")
-Pack:Require("Data/TehsTrails/Scripts/follower.lua")
 Pack:Require("Data/TehsTrails/Scripts/bounce.lua")
 Pack:Require("Data/TehsTrails/Scripts/highlight.lua")
 Pack:Require("Data/TehsTrails/Scripts/mmtoggle.lua")
@@ -16,37 +14,32 @@ Pack:Require("Data/TehsTrails/Scripts/globalmarker.lua")
 Pack:Require("Data/TehsTrails/Scripts/staticcategories.lua")
 Pack:Require("Data/TehsTrails/Scripts/copy.lua")
 Pack:Require("Data/TehsTrails/Scripts/skyscaleinfo.lua")
+Pack:Require("Data/TehsTrails/Scripts/follower.lua")
 Pack:Require("Data/TehsTrails/Scripts/tehmenu.lua")
 --Pack:Require("Data/TehsTrails/Scripts/globalconverter.lua")
 
 Teh_ChangeColor(Teh.storage.trailColor)
 
-function Teh_Tick_Handler(gameTime)
+local function tickHandler(gameTime)
 
     if (Teh.highlight.waypointHighlighted) then
-        if (Teh_Validate_Highlight()) then
-            Teh_Tick_Highlight(gameTime)
+        if (Teh_ValidateHighlight()) then
+            Teh_HighlightTickHandler(gameTime)
         end
     end
 
     if (Teh.heartfollower.isFollowing) then
-        Teh_Tick_HeartFollower()
+        Teh_FollowerTickHandler()
     end
 
     if (Teh.bounce.isBouncing) then
-        Teh_Tick_Bounce(gameTime)
+        Teh_BounceTickHandler(gameTime)
     end
 
-    if (not Teh.static.ran) then
-        Teh_Static_Categories(Mumble.CurrentMap.Id)
-    end
-
-    if (not Teh.copy.preloaded) then
-        Teh_Preload_Copy()
-    else Teh_Copy_Tick_Handler() end
+    Teh_CopyTickHandler()
 
 end
 
 --Teh_Convert_Markers()
 
-Event:OnTick(Teh_Tick_Handler)
+Event:OnTick(tickHandler)
