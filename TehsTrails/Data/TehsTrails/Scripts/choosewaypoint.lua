abff43431b0b3a965e9322471171b92a2966498a
xrandox <xrandox17@yahoo.com>
2023-08-28 20:21:12 -0500

    lanterns first pass done

--- TehsTrails/Data/TehsTrails/Scripts/choosewaypoint.lua
+++ TehsTrails/Data/TehsTrails/Scripts/choosewaypoint.lua
@@ -1,6 +1,7 @@
 Teh.choosewaypoint = {
     chooseableWaypoints = {},
     isRunning = false,
+    runningSet = nil,
     currentSize = 75,
     sizeGrowing = true
 }
@@ -9,29 +10,36 @@ Debug:Watch("Teh_ChooseWaypoint", Teh.choosewaypoint)
 local MAX_SIZE = 150
 local MIN_SIZE = 75
 
-local function startWatcher(marker)
-    if (next(Teh.choosewaypoint.chooseableWaypoints) == nil) then
-        marker:GetBehavior("InfoModifier").InfoValue = "You're done! (Hopefully...)"
+local function startWatcher(marker, waypointSet)
+    if (next(Teh.choosewaypoint.chooseableWaypoints[waypointSet]) == nil) then
+        if (waypointSet == 3) then
+            Teh_HighlightWaypoint(marker, true, "AkKsZ7g6w06leMCc8LTD7A==")
+            marker:GetBehavior("InfoModifier").InfoValue = "Now teleport to Bastion of the Celestial to finish out the routes"
+        else
+            marker:GetBehavior("InfoModifier").InfoValue = "You're done! (Hopefully...)"
+        end
         return
     end
     Teh.choosewaypoint.isRunning = true
+    Teh.choosewaypoint.runningSet = waypointSet
 end
 
 local function stopWatcher(marker)
     Teh.choosewaypoint.isRunning = false
     Teh.choosewaypoint.currentSize = MIN_SIZE
     Teh.choosewaypoint.sizeGrowing = true
-    Teh.choosewaypoint.chooseableWaypoints[marker.Guid] = nil
+    Teh.choosewaypoint.chooseableWaypoints[Teh.choosewaypoint.runningSet][marker.Guid] = nil
     marker:Remove()
 
-    for _, m in pairs(Teh.choosewaypoint.chooseableWaypoints) do
+    for _, m in pairs(Teh.choosewaypoint.chooseableWaypoints[Teh.choosewaypoint.runningSet]) do
         m.MapDisplaySize = MIN_SIZE * Mumble.UI.MapScale
         m.MapVisibility = false
     end
+    Teh.choosewaypoint.runningSet = nil
 end
 
 local function waypointWatcher()
-    for _, marker in pairs(Teh.choosewaypoint.chooseableWaypoints) do
+    for _, marker in pairs(Teh.choosewaypoint.chooseableWaypoints[Teh.choosewaypoint.runningSet]) do
         if marker.Focused then
             stopWatcher(marker)
             return
@@ -56,7 +64,7 @@ local function waypointAnimator(gameTime)
     if (size < MIN_SIZE) then growing = true end
     Teh.choosewaypoint.sizeGrowing = growing
 
-    for _, marker in pairs(Teh.choosewaypoint.chooseableWaypoints) do
+    for _, marker in pairs(Teh.choosewaypoint.chooseableWaypoints[Teh.choosewaypoint.runningSet]) do
         marker.MapVisibility = true
         marker.MapDisplaySize = size * Mumble.UI.MapScale
     end
@@ -69,13 +77,16 @@ local function chooseWaypointTickHandler(gameTime)
     end
 end
 
-function Teh_ChooseWaypointMarker(marker)
-    Teh.choosewaypoint.chooseableWaypoints[marker.Guid] = marker
+function Teh_ChooseWaypointMarker(marker, waypointSet)
+    if (Teh.choosewaypoint.chooseableWaypoints[waypointSet] == nil) then
+        Teh.choosewaypoint.chooseableWaypoints[waypointSet] = {}
+    end
+    Teh.choosewaypoint.chooseableWaypoints[waypointSet][marker.Guid] = marker
 end
 
-function Teh_ChooseWaypointTrigger(marker, isfocused)
+function Teh_ChooseWaypointTrigger(marker, isfocused, waypointSet)
     if (isfocused and Teh.choosewaypoint.isRunning == false) then
-        startWatcher(marker)
+        startWatcher(marker, waypointSet)
     end
 end
 
