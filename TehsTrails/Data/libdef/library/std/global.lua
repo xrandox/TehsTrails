4ef172951fef81cd5dd071abc8083389f89b7d19
xrandox <xrandox17@yahoo.com>
2025-07-19 16:45:37 -0500

    v6k update

--- TehsTrails/Data/libdef/library/std/global.lua
+++ TehsTrails/Data/libdef/library/std/global.lua
@@ -2,3 +2,6 @@
 
 ---@deprecated # This is not supported.  Use Pack:Require(path) instead.
 function _G.require(path) end
+
+---@type string # This provides the current version of the Pathing module.
+PathingVersion = PathingVersion or "0.0.0"
